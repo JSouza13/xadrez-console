70f5af0d2cf0747f3fc855a490e948ce962428cd
João Vitor <jooao_souza@hotmail.com>
2019-01-17 13:49:53 -0200

    Iniciado a implementação do método abstrato movimentosPossiveis

--- xadrez-console/xadrez/Rei.cs
+++ xadrez-console/xadrez/Rei.cs
@@ -2,15 +2,85 @@
 
 namespace xadrez
 {
-    class Rei : Peca
+    internal class Rei : Peca
     {
-       public Rei(Tabuleiro tabuleiro, Cor cor) : base(cor, tabuleiro)
+        public Rei(Tabuleiro tabuleiro, Cor cor) : base(cor, tabuleiro)
         {
         }
 
         public override string ToString()
         {
             return "R";
+        }
+
+        private bool podeMover(Posicao pos)
+        {
+            Peca p = Tab.peca(pos);
+            return p == null || p.Cor != Cor;
+        }
+
+        public override bool[,] movimentosPossiveis()
+        {
+            bool[,] matriz = new bool[Tab.Linhas, Tab.Colunas];
+
+            Posicao pos = new Posicao(0, 0);
+
+            // Acima
+            pos.definirValores(Posicao.Linha - 1, Posicao.Coluna);
+            if (Tab.posicaoValida(pos) && podeMover(pos))
+            {
+                matriz[pos.Linha, pos.Coluna] = true;
+            }
+
+            // Nordeste
+            pos.definirValores(Posicao.Linha - 1, Posicao.Coluna + 1);
+            if (Tab.posicaoValida(pos) && podeMover(pos))
+            {
+                matriz[pos.Linha, pos.Coluna] = true;
+            }
+
+            // Direita
+            pos.definirValores(Posicao.Linha, Posicao.Coluna + 1);
+            if (Tab.posicaoValida(pos) && podeMover(pos))
+            {
+                matriz[pos.Linha, pos.Coluna] = true;
+            }
+
+            // Sudeste
+            pos.definirValores(Posicao.Linha + 1, Posicao.Coluna + 1);
+            if (Tab.posicaoValida(pos) && podeMover(pos))
+            {
+                matriz[pos.Linha, pos.Coluna] = true;
+            }
+
+            // Abaixo
+            pos.definirValores(Posicao.Linha + 1, Posicao.Coluna);
+            if (Tab.posicaoValida(pos) && podeMover(pos))
+            {
+                matriz[pos.Linha, pos.Coluna] = true;
+            }
+
+            // Sudoeste
+            pos.definirValores(Posicao.Linha + 1, Posicao.Coluna - 1);
+            if (Tab.posicaoValida(pos) && podeMover(pos))
+            {
+                matriz[pos.Linha, pos.Coluna] = true;
+            }
+
+            // Esquerda
+            pos.definirValores(Posicao.Linha, Posicao.Coluna - 1);
+            if (Tab.posicaoValida(pos) && podeMover(pos))
+            {
+                matriz[pos.Linha, pos.Coluna] = true;
+            }
+
+            // Noroeste
+            pos.definirValores(Posicao.Linha - 1, Posicao.Coluna - 1);
+            if (Tab.posicaoValida(pos) && podeMover(pos))
+            {
+                matriz[pos.Linha, pos.Coluna] = true;
+            }
+            return matriz;
         }
     }
 }
--- xadrez-console/xadrez/Torre.cs
+++ xadrez-console/xadrez/Torre.cs
@@ -12,5 +12,67 @@ namespace xadrez
         {
             return "T";
         }
+
+        private bool podeMover(Posicao pos)
+        {
+            Peca p = Tab.peca(pos);
+            return p == null || p.Cor != Cor;
+        }
+
+        public override bool[,] movimentosPossiveis()
+        {
+            bool[,] matriz = new bool[Tab.Linhas, Tab.Colunas];
+
+            Posicao pos = new Posicao(0, 0);
+
+            // Acima
+            pos.definirValores(Posicao.Linha - 1, Posicao.Coluna);
+            while(Tab.posicaoValida(pos) && podeMover(pos))
+            {
+                matriz[pos.Linha, pos.Coluna] = true;
+                if(Tab.peca(pos) != null && Tab.peca(pos).Cor != Cor)
+                {
+                    break;
+                }
+                pos.Linha = pos.Linha - 1;
+            }
+
+            // Direita
+            pos.definirValores(Posicao.Linha - 1, Posicao.Coluna);
+            while (Tab.posicaoValida(pos) && podeMover(pos))
+            {
+                matriz[pos.Linha, pos.Coluna] = true;
+                if (Tab.peca(pos) != null && Tab.peca(pos).Cor != Cor)
+                {
+                    break;
+                }
+                pos.Linha = pos.Linha - 1;
+            }
+
+            // Abaixo
+            pos.definirValores(Posicao.Linha + 1, Posicao.Coluna);
+            while (Tab.posicaoValida(pos) && podeMover(pos))
+            {
+                matriz[pos.Linha, pos.Coluna] = true;
+                if (Tab.peca(pos) != null && Tab.peca(pos).Cor != Cor)
+                {
+                    break;
+                }
+                pos.Linha = pos.Linha + 1;
+            }
+
+            // Esquerda
+            pos.definirValores(Posicao.Linha - 1, Posicao.Coluna);
+            while (Tab.posicaoValida(pos) && podeMover(pos))
+            {
+                matriz[pos.Linha, pos.Coluna] = true;
+                if (Tab.peca(pos) != null && Tab.peca(pos).Cor != Cor)
+                {
+                    break;
+                }
+                pos.Linha = pos.Linha - 1;
+            }
+            return matriz;
+        }
     }
 }
