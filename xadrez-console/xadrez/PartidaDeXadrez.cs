a1c4d6bc5e26b348453adfb6632c3d215ef0b97a
Joao Vitor de Souza <jooao_souza@hotmail.com>
2019-01-16 22:12:49 -0200

    Começando a implementar a classe PartidaDeXadrez

---
+++ xadrez-console/xadrez/PartidaDeXadrez.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using tabuleiro;
+
+namespace xadrez
+{
+    class PartidaDeXadrez
+    {
+        public Tabuleiro Tabuleiro { get; set; }
+        public int Turno { get; set; }
+        public Cor JogadorAtual { get; set; }
+
+        public PartidaDeXadrez()
+        {
+            Tabuleiro = new Tabuleiro(8,8);
+            Turno = 1;
+            JogadorAtual = Cor.Branca;
+        }
+
+        public void executaMovimento(Posicao origem, Posicao destino)
+        {
+            Peca p = tabuleiro.re
+        }
+    }
+}
